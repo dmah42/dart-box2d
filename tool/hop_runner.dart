2a82c85c7dfb89b5a1eacf0e98c71c5b857192a6
Kevin Moore <github@j832.com>
2014-01-28 15:51:15 -0800

    build all demos with hop

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -4,10 +4,12 @@ import 'dart:async';
 import 'dart:io';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
-
+import 'package:path/path.dart' as pathos;
 
 void main(List<String> args) {
   addTask('analyze_libs', createAnalyzerTask(_getLibs));
+
+  addTask('dart2js', createDartCompilerTask(_getDemos(), liveTypeAnalysis: true));
 
   runHop(args);
 }
@@ -18,3 +20,12 @@ Future<List<String>> _getLibs() {
       .map((File file) => file.path)
       .toList();
 }
+
+Future<List<String>> _getDemos() =>
+  new Directory('example/demos')
+    .list()
+    .where((FileSystemEntity fse) => fse is File)
+    .map((File f) => f.path)
+    .where((String path) => pathos.extension(path) == '.dart')
+    .where((String path) => pathos.basename(path) != 'demo_harness.dart')
+    .toList();
